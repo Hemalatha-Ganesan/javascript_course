ffb820d2a2d0c890601f1e37b12e089872f656cc
Hemalatha Ganesan <hemalathaganesan08@gmail.com>
2025-12-05 12:50:47 +0530

    first commit

---
+++ Introduction/script.js
@@ -0,0 +1,18 @@
+console.log("hello world");
+       // this is an alert
+       // variables - 8 bytes - dynamically typed language
+       let score =  0; // initialization
+       score = 100; // assignment
+       console.log("The score is:" + score);
+       const pi = 3.14;
+       console.log("The pi value is:" + pi)
+
+       //user input
+// let name = prompt("Enter your name:")
+// console.log("Hello", name);
+
+// let tickets = prompt("Enter no of tickets:");
+// console.log("You should pay:", Number(tickets) * 190);
+
+let a = prompt("Enter value of a:")
+console.log(Number(a)+10);
